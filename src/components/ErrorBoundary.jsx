99bca06e92194d3a0c57e52280e3dc4f27e426c2
J0NNYZER0 <jon.ortiz@me.com>
2019-03-11 16:17:11 -0700

    fix error boundary

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -5,16 +5,16 @@ class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentError: null,
-      errorInfo: null,
+      error: null,
+      stack: null,
     };
   }
 
   componentDidCatch(error, info) {
     // You can also log the error to an error reporting service
     this.setState({
-      currentError: error,
-      errorInfo: info,
+      error: error,
+      stack: info.componentStack,
     });
 
     if (window.rg4js) {
@@ -23,16 +23,19 @@ class ErrorBoundary extends Component {
   }
 
   render() {
-    const { currentError, errorInfo } = this.state;
+    const { error, stack } = this.state;
     const { children } = this.props;
 
-    if (currentError) {
+    if (error) {
       // You can render any custom fallback UI
       return (
         <div className="error">
-          <h1>Something went wrong.</h1>
-          <div>{currentError}</div>
-          <div>{errorInfo}</div>
+          <h4>Something went wrong</h4>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {error.toString()}
+            <br />
+            {stack}
+          </details>
         </div>
       );
     }
